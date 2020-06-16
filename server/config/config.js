3ab4ed072946b083039676d842e2bf7a32bb140d
maximiliano.gnovatto <maxignov@hotmail.com>
2020-06-16 01:07:44 -0300

    Rest server - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://cafe-user:gGN3kMejLVrqHsLw@cluster0-12ypw.mongodb.net/cafe'
+    urlDB = 'mongodb+srv://<user>:<pass>@cluster0-12ypw.mongodb.net/cafe'
 }
 
 process.env.URLDB = urlDB;
